006a69a41b8faa0a078a9fcc7de8e118eed49aa9
Shreyan <shreyanacharjee21@gmail.com>
2025-04-09 05:07:10 +0530

    chnaged

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL
+  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
 
   withCredentials: true,
 });
