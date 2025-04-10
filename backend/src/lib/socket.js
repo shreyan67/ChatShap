28404780f7ad3f342d40b3a23507737fce700fbf
Shreyan <shreyanacharjee21@gmail.com>
2025-04-10 11:40:22 +0530

    added

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://chat-shap-9aar.vercel.app/"],
   },
 });
 
